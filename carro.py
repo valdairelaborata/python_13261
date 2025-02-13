4c4166667eb0f887cd486aecd37693fb4741b923
phantodev <kduburko@gmail.com>
2025-02-13 20:03:48 -0300

    lab_01

--- carro.py
+++ carro.py
@@ -1,9 +1,28 @@
 class Carro:
-    def __init__(self, cor):        
+    def __init__(self, cor, placa, status, tipo):        
         self.cor = cor
+        self.placa = placa
+        self.status = status
+        self.tipo = tipo
+    
+    def vender(self):
+        self.status = "Vendido"
 
+    def ligar(self):
+        self.status = "Ligado"
 
-carro = Carro("Preta")
+    def desligar(self):
+        self.status = "Desligado"
+
+    def acelerar_frente(self):
+        pass
+
+    
+
+carro = Carro("Preta", "AKS-2563", "Desligado", "Hatch")
+carro.ligar()
+carro.desligar()
+
 
 print("Fim")
 
