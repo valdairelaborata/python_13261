c7f9c9199e01341c701508acda8d97d09aaac6d2
phantodev <kduburko@gmail.com>
2025-02-18 19:12:17 -0300

    aula 02

--- carro.py
+++ carro.py
@@ -6,6 +6,9 @@ class Carro:
 
     def descricao(self):
         return f"{self.marca} - {self.modelo} - {self.ano}"
+
+    def acelerar(self):
+        return f"Acelerar o carro {self.modelo}"
     
 class CarroEsportivo(Carro):
     def __init__(self, marca, modelo, ano, velocidade_maxima):
@@ -15,9 +18,14 @@ class CarroEsportivo(Carro):
     def descricao(self):
         return super().descricao() + f" {self.velocidade_maxima}"
 
+    def  acelerar(self):
+        return super().acelerar()
+
+
+    
 class CarroSedan(Carro):
     def __init__(self, marca, modelo, ano, tamanho_porta_malas):
-        super().__init__(marca, modelo, ano)
+        super().__init__(marca, modelo, ano)        
         self.tamanho_porta_malas = tamanho_porta_malas
 
     def descricao(self):
@@ -25,11 +33,11 @@ class CarroSedan(Carro):
    
 carro_esportivo = CarroEsportivo("Honda", "Civic GTI", "2003", 250)
 descricao_carro_esportivo = carro_esportivo.descricao()
-
+carro_esportivo.acelerar()
 
 carro_sedan = CarroSedan("Honda", "Civic", "2003", 370)
 descricao_carro_sedan = carro_sedan.descricao()
-
+carro_sedan.acelerar()
 
 print("Fim")
 
