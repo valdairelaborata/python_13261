99ce613ddb44a7906a9e54f0229b8875328b85f1
phantodev <kduburko@gmail.com>
2025-02-20 20:32:23 -0300

    lab_02

--- lab_02.py
+++ lab_02.py
@@ -1,35 +1,59 @@
-# 2 - Crie uma classe Calculadora com métodos para adição, subtração, multiplicação e divisão. Crie uma instância da classe e realize algumas operações.
+# 2 - Crie uma classe ContaBancaria que inclui funcionalidades básicas e uma classe ContaCorrente que herda da classe base. A classe derivada ContaCorrente substitui o método sacar para considerar um limite de saque além do saldo disponível.
 
-class Calculadora: 
-    def __init__(self, resultado = 0):
-        self.resultado = resultado
+class ContaBancaria:
+    def __init__(self, titular, numeroConta):
+        self.__titular = titular
+        self.__numeroConta = numeroConta
+        self.__saldo = 0    
+ 
+    @property
+    def titular(self):
+        return self.__titular
+ 
+    @property
+    def saldo(self):
+        return self.__saldo
+       
+    def trocar_titular(self, novo_titular):
+        self.__titular = novo_titular
 
-    def adicao(self, valor):
-        self.resultado += valor
+    def saque(self, valor):
+        self.__saldo -= valor
+ 
+    def deposito(self, valor):
+        self.__saldo += valor
+class ContaCorrente(ContaBancaria):
+    def __init__(self, titular, numeroConta, limite):
+        super().__init__(titular, numeroConta)
+        self.__limite = limite
+   
+    @property
+    def limite(self):
+        return self.__limite
+ 
+    def saque(self, valor_do_saque):
 
-    def subtracao(self, valor):
-        self.resultado -= valor
+       if self.saldo >= valor_do_saque:
+            super().saque(valor_do_saque)
+       else:
+           print("Saldo insuficiente para o saque")
     
-    def multiplicacao(self, valor):
-        self.resultado *= valor
-
-    def divisao(self, valor):
-        self.resultado /= valor
-    
-    def exibir_resultado(self):
-        return self.resultado
+    @property
+    def saldo(self):
+        return super().saldo + self.limite
 
 
-calculadora = Calculadora()
-
-calculadora.adicao(10)
-calculadora.adicao(5)
-calculadora.subtracao(15)
-calculadora.adicao(50)
-calculadora.divisao(5)
-calculadora.multiplicacao(2)
-
-resultado = calculadora.exibir_resultado()
-
-
-print("Fim")
+conta_corrente = ContaCorrente("Atila", "501025", 500)
+conta_corrente.deposito(500)
+saldo = conta_corrente.saldo
+conta_corrente.trocar_titular("Ana")
+titular = conta_corrente.titular
+conta_corrente.saque(600)
+saldo = conta_corrente.saldo
+conta_corrente.saque(500)
+conta_corrente.deposito(150)
+conta_corrente.saque(500)
+saldo = conta_corrente.saldo
+ 
+print(f"seu novo saldo é ")
+ 
