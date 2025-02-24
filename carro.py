3578811b7b226092f0357abbde060fb953e91641
phantodev <kduburko@gmail.com>
2025-02-24 20:50:15 -0300

    aula_03

--- carro.py
+++ carro.py
@@ -1,4 +1,4 @@
-class Carro: 
+class Carro(object): 
     def __init__(self, marca, modelo, ano):        
         self.marca = marca
         self.modelo = modelo
@@ -9,35 +9,67 @@ class Carro:
 
     def acelerar(self):
         return f"Acelerar o carro {self.modelo}"
+
+    def __str__(self):
+        return f"Carro ano: {self.ano}, modelo: {self.modelo}"
+
+    def __repr__(self):
+        return f"Carro(modelo={self.modelo})"
+
+    def __del__(self):
+        log = f"O objeto {self.modelo} deixou de existir"
+        print("")
     
-class CarroEsportivo(Carro):
-    def __init__(self, marca, modelo, ano, velocidade_maxima):
-        super().__init__(marca, modelo, ano)
-        self.velocidade_maxima = velocidade_maxima
+# carro = Carro("Honda", "Civic", "2003")
+# carro.acelerar()
+# representacao_carro = repr(carro)
+# print(carro)        
+# del carro
 
-    def descricao(self):
-        return super().descricao() + f" {self.velocidade_maxima}"
+# class Numero:
+#     def __init__(self, valor):
+#         self.valor = valor
 
-    def  acelerar(self):
-        return super().acelerar()
+#     def __add__(self, outro_objeto):
+#         soma_dos_objetos = self.valor + outro_objeto.valor
+#         return Numero(soma_dos_objetos)
+
+# numero1 = Numero(10)
+# numero2 = Numero(20)
+# resultado_da_soma = numero1 + numero2
 
 
+
+# class Pessoa:
+#     def __init__(self, nome, idade):
+#         self.nome = nome
+#         self.idade = idade
+
+#     def __eq__(self, outra_pessoa):
+#         return self.nome == outra_pessoa.nome and self.idade == outra_pessoa.idade
+
+#     def __ne__(self, outra_pessoa):
+#         return not self.__eq__(outra_pessoa)
+
+# pessoa1 = Pessoa("Ana", 21)
+# pessoa2 = Pessoa("Ana", 21)
+
+# sao_iguais = (pessoa1 == pessoa2)
+
+# nao_sao_iguais = (pessoa1 != pessoa2)
+
+
+class ListaPersonalizada:
+    def __init__(self, itens):
+        self.itens = itens
     
-class CarroSedan(Carro):
-    def __init__(self, marca, modelo, ano, tamanho_porta_malas):
-        super().__init__(marca, modelo, ano)        
-        self.tamanho_porta_malas = tamanho_porta_malas
+    def __len__(self):
+        return len(self.itens)
 
-    def descricao(self):
-        return super().descricao() + f" {self.tamanho_porta_malas}"
-   
-carro_esportivo = CarroEsportivo("Honda", "Civic GTI", "2003", 250)
-descricao_carro_esportivo = carro_esportivo.descricao()
-carro_esportivo.acelerar()
 
-carro_sedan = CarroSedan("Honda", "Civic", "2003", 370)
-descricao_carro_sedan = carro_sedan.descricao()
-carro_sedan.acelerar()
+lista = ListaPersonalizada([1,2,3,4,5])
+
+quantos_itens_tem_na_lista = len(lista)
 
 print("Fim")
 
