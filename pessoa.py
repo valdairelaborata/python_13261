49d144b8f5ee1c591874ce075522ba5722e4cf55
phantodev <kduburko@gmail.com>
2025-02-28 20:27:49 -0300

    lab_01

--- pessoa.py
+++ pessoa.py
@@ -9,28 +9,28 @@ class Pessoa:
     def apresentar(self):
         return f"Olá, eu sou o {self.nome}, tenho {self.idade} anos e sou do gênero {self.genero}"
 
-    # def eh_maior_de_idade(self):
-    #     return self.idade >= 18
-
+    def eh_maior_de_idade(self):
+        return self.idade >= 18
+    
+    def eh_menor_de_idade(self):
+        return self.idade < 18
+    
 
 class TestePessoa(unittest.TestCase):
-   
-   def eh_maior_de_idade(self):
+    def teste_apresentar(self):
         pessoa = Pessoa("Ana", 20, "Feminino")
-        eh_maior_de_idade = pessoa.eh_maior_de_idade()
-        self.assertTrue(eh_maior_de_idade)
-    
-    # def teste_apresentar(self):
-    #     pessoa = Pessoa("Ana", 20, "Feminino")
-    #     apresentacao = pessoa.apresentar()
-    #     # Olá, eu sou o Ana, tenho 20 anos e sou do gênero Feminino
-    #     # print(apresentacao)
-    #     self.assertEqual(apresentacao, "Olá, eu sou o Ana, tenho 20 anos e sou do gênero Feminino")
-    
-    # def eh_maior_de_idade(self):
-    #     pessoa = Pessoa("Ana", 20, "Feminino")
-    #     eh_maior_de_idade = pessoa.eh_maior_de_idade()
-    #     self.assertTrue(eh_maior_de_idade)
+        apresentacao = pessoa.apresentar()
+        # Olá, eu sou o Ana, tenho 20 anos e sou do gênero Feminino
+        # print(apresentacao)
+        self.assertEqual(apresentacao, "Olá, eu sou o Ana, tenho 20 anos e sou do gênero Feminino")
+
+    def teste_eh_maior_de_idade(self):
+        pessoa = Pessoa("Ana", 20, "Feminino")
+        self.assertTrue(pessoa.eh_maior_de_idade())
+
+    def teste_eh_menor_de_idade(self):
+        pessoa = Pessoa("Ana", 17, "Feminino")
+        self.assertTrue(pessoa.eh_menor_de_idade())
 
 
 unittest.main()
