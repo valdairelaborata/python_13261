9d8673df2c9e3c60def3b61c881213d189a5ac9d
phantodev <kduburko@gmail.com>
2025-03-07 20:59:54 -0300

    Lab_02

--- sql_alchemy.py
+++ sql_alchemy.py
@@ -12,8 +12,26 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 db = SessionLocal()
 
-novo_cliente = Cliente(nome = "Paulo", email="paulo@gmail.com")
-db.add(novo_cliente)
+# novo_cliente = Cliente(nome = "Paulo", email="paulo@gmail.com")
+# db.add(novo_cliente)
+# db.commit()
+
+# clientes = db.query(Cliente).all()
+
+# for cliente in clientes:
+#     print(f"Nome: {cliente.nome} - E-mail: {cliente.email}")
+
+
+# select * from cliente c where c.id = 2
+
+cliente = db.query(Cliente).filter(Cliente.id == 2).first()
+cliente.nome = "Claudio"
+cliente.email = "claudio@gmail.com"
+
+
+cliente = db.query(Cliente).filter(Cliente.id == 2).first()
+db.delete(cliente)
 db.commit()
+
 
 db.close()
